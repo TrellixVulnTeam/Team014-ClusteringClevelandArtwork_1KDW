e85c0f0570c2baab3af995d62150ff1f6615119a
Hannah Li <li.hannah@gmail.com>
2021-12-02 07:14:29 -0500

    minor changes to prev

--- cluster-art/src/App.js
+++ cluster-art/src/App.js
@@ -11,12 +11,7 @@ class App extends React.Component {
 
   componentDidMount() {
     this.drawChart();
-    //this.drawNode();
   }
-
-  /*componentDidUpdate() {
-    
-  }*/
 
   drawChart() {
     const svg = d3.select(this.refs.space)
@@ -29,83 +24,6 @@ class App extends React.Component {
                 .style('-khtml-user-select', 'none')
                 .style('-moz-user-select', 'none')
                 .style('-ms-user-select', 'none')
-  }
-
-  drawNode() {
-    var d = d3.csv(data, function(d) {
-      return {
-        x: d.x,
-        y: d.y,
-        text: d.text
-      }
-    }).then(function(d) {
-      console.log(d);
-    });
-
-    var div = d3.select(this.refs.space).append('g');
-
-    var textbox = div.append('rect')
-      .attr('x', '0')
-      .attr('y', '0')
-      .attr('width', 90)
-      .attr('height', 70)
-      .attr('fill', '#e28743')
-      .attr('stroke', '#e28743')
-      .style("opacity", 0);
-
-    var text = div.append('text')
-      .attr('dy', 35)
-      .attr('dx', 10)
-      .attr('padding', 10)
-      .attr("dominant-baseline", "middle")
-      .attr("text-anchor", "start")
-      .attr("fill", "pink")
-      .attr("font-family", "sans-serif")
-      .attr("font-size", "20px")
-      .style("opacity", 0);
-
-    div.selectAll('rect')
-    .attr("width", function(d) {return this.parentNode.getBBox().width + 10;})
-
-
-    /*const circle = d3.select(this.refs.space)
-                    .append('circle')
-                    .attr('cx', function() { return 200; })
-                    .attr('cy', function() { return 200; })
-                    .attr('fill', function() { return 'yellow'; })
-                    .attr('r', 5)
-                    .on("mouseover", function(){
-                      textbox.style("opacity", 1)
-                      .attr('x', 200 + 10)
-                      .attr('y', 200 + 10)
-                      .transition()
-                      .duration('50');
-
-                      text.style("opacity", 1)
-                      .attr('x', 200 + 10)
-                      .attr('y', 200 + 10)
-                      .transition()
-                      .duration('50');
-                    })
-                    .on("mouseout", function(){
-                      textbox.transition()
-                         .duration('50')
-                         .style("opacity", 0);
-                      text.transition()
-                         .duration('50')
-                         .style("opacity", 0);
-                    });*/
-
-    div.selectAll('circle')
-          .data(d)
-          .enter()
-          .append('circle')
-          .attr('cx', function(d) { console.log(d.x); return d.x; })
-          .attr('cy', function(d) { return d.y; })
-          .attr('fill', function() { return 'yellow'; })
-          .attr('r', 5);
-
-
   }
 
   render() {
