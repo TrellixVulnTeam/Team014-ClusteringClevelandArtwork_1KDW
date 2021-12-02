5e1d74d849a5738e525c6c37ed3af67539124cb3
Hannah Li <li.hannah@gmail.com>
2021-12-02 07:13:39 -0500

    multiple nodes with textboxes

--- cluster-art/src/App.js
+++ cluster-art/src/App.js
@@ -2,12 +2,21 @@ import './App.css';
 import * as d3 from 'd3'
 import React, { Component } from 'react'
 
-class App extends Component {
+import data from './data.csv';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
   componentDidMount() {
     this.drawChart();
-    this.drawNode();
-  }  
+    //this.drawNode();
+  }
+
+  /*componentDidUpdate() {
+    
+  }*/
 
   drawChart() {
     const svg = d3.select(this.refs.space)
@@ -23,6 +32,16 @@ class App extends Component {
   }
 
   drawNode() {
+    var d = d3.csv(data, function(d) {
+      return {
+        x: d.x,
+        y: d.y,
+        text: d.text
+      }
+    }).then(function(d) {
+      console.log(d);
+    });
+
     var div = d3.select(this.refs.space).append('g');
 
     var textbox = div.append('rect')
@@ -35,7 +54,6 @@ class App extends Component {
       .style("opacity", 0);
 
     var text = div.append('text')
-      .text(function () { return "testingggggggggggggg" })
       .attr('dy', 35)
       .attr('dx', 10)
       .attr('padding', 10)
@@ -50,15 +68,13 @@ class App extends Component {
     .attr("width", function(d) {return this.parentNode.getBBox().width + 10;})
 
 
-    const circle = d3.select(this.refs.space)
+    /*const circle = d3.select(this.refs.space)
                     .append('circle')
                     .attr('cx', function() { return 200; })
                     .attr('cy', function() { return 200; })
                     .attr('fill', function() { return 'yellow'; })
                     .attr('r', 5)
                     .on("mouseover", function(){
-                      console.log("mouseover");
-
                       textbox.style("opacity", 1)
                       .attr('x', 200 + 10)
                       .attr('y', 200 + 10)
@@ -72,20 +88,101 @@ class App extends Component {
                       .duration('50');
                     })
                     .on("mouseout", function(){
-                      console.log("mouseout");
                       textbox.transition()
                          .duration('50')
                          .style("opacity", 0);
                       text.transition()
                          .duration('50')
                          .style("opacity", 0);
-                    });
+                    });*/
+
+    div.selectAll('circle')
+          .data(d)
+          .enter()
+          .append('circle')
+          .attr('cx', function(d) { console.log(d.x); return d.x; })
+          .attr('cy', function(d) { return d.y; })
+          .attr('fill', function() { return 'yellow'; })
+          .attr('r', 5);
+
+
   }
 
   render() {
+    var d = d3.csv(data, function(d) {
+      return {
+        x: +d.x,
+        y: +d.y,
+        text: d.text.toString()
+      }
+    }).then(dt => {
+      console.log(dt);
+
+      var div = d3.select(this.refs.space).append('g');
+
+      var textbox = div.append('rect')
+        .attr('x', '0')
+        .attr('y', '0')
+        .attr('width', 90)
+        .attr('height', 70)
+        .attr('fill', '#e28743')
+        .attr('stroke', '#e28743')
+        .style("opacity", 0);
+
+      var text = div.append('text')
+        .attr('dy', 35)
+        .attr('dx', 10)
+        .attr('padding', 10)
+        .attr("dominant-baseline", "middle")
+        .attr("text-anchor", "start")
+        .attr("fill", "pink")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "20px")
+        .style("opacity", 0);
+
+      div.selectAll('rect')
+      .attr("width", function(d) {return this.parentNode.getBBox().width + 10;})
+
+      div.selectAll('circle')
+          .data(dt)
+          .enter()
+          .append('circle')
+          .attr('class',  function(d) { return d.text; })
+          .attr('cx', function(d) { return d.x; })
+          .attr('cy', function(d) { return d.y; })
+          .attr('fill', function() { return 'yellow'; })
+          .attr('r', 5)
+          .on("mouseover", function(d){
+            text.style("opacity", 1)
+            .text(d['target'].__data__.text)
+            .attr('x', d.x + 10)
+            .attr('y', d.y + 10)
+            .transition()
+            .duration('50');
+
+            var text_width = text.node().getBBox().width + 20;
+
+            textbox.style("opacity", 1)
+            .attr("width", function(d) {return text_width;})
+            .attr('x', d.x + 10)
+            .attr('y', d.y + 10)
+            .transition()
+            .duration('50');
+          })
+          .on("mouseout", function(d){
+            textbox.transition()
+              .attr("width", function(d) {return 0;})
+               .duration('50')
+               .style("opacity", 0);
+            text.transition()
+               .duration('50')
+               .style("opacity", 0);
+          });
+    });
+
     return (
-      <div ref=''>
-        <svg ref ="space">
+      <div>
+        <svg ref='space'>
         </svg>
       </div>
     );
