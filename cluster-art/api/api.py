1f492a52086183624564ddaa815751b3c62c221a
Harshal <hdahake3@gatech.edu>
2021-12-05 17:14:11 -0500

    Testing Kmeans

--- cluster-art/api/api.py
+++ cluster-art/api/api.py
@@ -1,6 +1,9 @@
 import time
 from flask import Flask, request
 from flask_cors import CORS
+from kmeans import ArtKMeans
+import sqlite3 as lite
+import pandas as pd
 
 app = Flask(__name__)
 CORS(app)
@@ -8,4 +11,12 @@ CORS(app)
 @app.route('/time', methods=['POST'])
 def get_current_time():
     print(request.get_json())
+    """
+    con = lite.connect('FinalProject.db')
+    df_artworks = pd.read_sql_query('SELECT * from artworks', con)
+
+    model = ArtKMeans()
+
+    model.fit()
+    """
     return {'time': time.time()}
